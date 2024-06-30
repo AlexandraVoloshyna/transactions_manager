4a551415a6dcf30e386cf67bab40d22af4dc67d1
AlexandraVoloshyna <oleksandravolosyna@gmail.com>
2024-06-30 19:10:27 +0300

    removed query client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ChakraProvider, theme } from '@chakra-ui/react';
 
 import { App } from './App.tsx';
 import './index.css';
-
-const queryClient = new QueryClient();
+import { queryClient } from './hooks';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
